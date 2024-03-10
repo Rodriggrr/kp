902087aa5af3e6ee30d5c5a2b8504848d002c0c0
Rodrigo Farinon <rfarinon@alu.ufc.br>
2024-03-10 16:12:48 -0300

    Fixed PATH

--- tools/install.sh
+++ tools/install.sh
@@ -8,29 +8,34 @@ pip install termcolor
 
 
 # Set the installation directory
-INSTALL_DIR=~/.local/bin
+INSTALL_DIR="~/.local/bin"
+
+# Check the installation directory
+if [ ! -d $INSTALL_DIR ]; then
+    mkdir -p $INSTALL_DIR
+fi
 
 # download kp
 curl -fsSL https://raw.githubusercontent.com/Rodriggrr/kp/main/kp.py > $INSTALL_DIR/kp
 
-# Mv the file
+# CHMOD the file
 chmod +x $INSTALL_DIR/kp
 
 # Check for PATH
 amarelo='\033[0;33m'
 vermelho='\033[0;31m'
 reset='\033[0m'
+answer=""
 
 status=$(echo $PATH | grep -o '/.local/bin' | wc -l)
 
 if [ $status = "0" ]; then
-	echo "${vermelho} ATENÇÃO: ${INSTALL_DIR} não existe na varíavel PATH. Deseja adicionar? (caso escolha não, o programa não irá funcionar) [Y/n]${reset}"
-    read -r answer
+	echo "${vermelho}ATENÇÃO: ${INSTALL_DIR} não existe na varíavel PATH. Deseja adicionar? Será necessário reiniciar o terminal. (caso escolha não, o programa não irá funcionar) [Y/n]${reset}"
+    read answer
 
-    if [ answer = "Y" ] || [ answer = "y" ]; then
-        echo "export PATH=$PATH:$INSTALL_DIR" >> ~/.bashrc
-        echo "export PATH=$PATH:$INSTALL_DIR" >> ~/.zshrc
-        export PATH=$PATH:$INSTALL_DIR
+    if [ $answer = "Y" ] || [ $answer = "y" ]; then
+        echo "export PATH=\$PATH:${INSTALL_DIR}" >> ~/.bashrc
+        echo "export PATH=\$PATH:${INSTALL_DIR}" >> ~/.zshrc
     fi
 fi 
 
